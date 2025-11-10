eaab89f89512a33c127f9872f5b452e823e938d5
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2025-11-10 08:56:57 +0200

    ldr

---
+++ ldr.ino
@@ -0,0 +1,21 @@
+int sensorPin = A0;   // select the input pin for the potentiometer
+int ledPin = 13;      // select the pin for the LED
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPin, OUTPUT);
+  Serial.println(9600);
+}
+
+void loop() {
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+  Serial.println(sensorValue);
+  delay(1000);
+  if (sensorValue < 500) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+}
