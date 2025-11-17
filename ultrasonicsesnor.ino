4a791af576bbd7369b55c6910300dfcea44761da
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2025-11-17 11:31:55 +0200

    ultrasonic sesnor

---
+++ ultrasonicsesnor.ino
@@ -0,0 +1,22 @@
+int echoPin = 10;
+int trigPin = 11;
+int maxDistance = 200;
+int distance;
+
+void setup() {
+  pinMode(echoPin, INPUT);
+  pinMode(trigPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  distance = pulseIn(echoPin, HIGH);
+  distance = distance / 58;
+  Serial.println(distance);
+  delay(100);
+}
